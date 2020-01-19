547f06087d278cd7887c1bd99034a7dd8408f8f3
Akhil Sasidharan <akhilsasidharan1991@gmail.com>
2020-01-19 16:31:30 +0530

    http server using NewServeMux

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", hello)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	http.ListenAndServe(":8080", mux)
 }
